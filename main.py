676f5f4ae61c1249196620b983b1875b45e5e281
tsr <tushar.soni@delhitechnicalcampus.ac.in>
2024-11-13 11:59:33 +0530

    update

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+
+from PyPDF2 import PdfReader
+
+import streamlit as st
+import os
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
 
 def get_pdf_text(pdf_docs):
     text = ""
