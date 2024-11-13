1cb112e9fdd66cb8125ea7e29d56826a2546aaa5
tsr <tushar.soni@delhitechnicalcampus.ac.in>
2024-11-13 19:50:19 +0530

    update

--- main.py
+++ main.py
@@ -117,8 +117,11 @@ def main():
     
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
+        api_key = st.text_input("Enter your Google API Key")
+        genai.configure(api_key=api_key)
+
+        pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
+        if st.button("Submit & Process") and pdf_docs:
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
